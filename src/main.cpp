e15474eea3b68f33c69d0e5ef3174b17aae9ea0d
Vitor Marinho Faria <vitorfariam97@gmail.com>
2022-04-29 17:02:36 -0300

    Better defaults. Show on screen keyboard when requested by ImGui. Use better fonts. ImGui settings saving and loading.

--- src/main.cpp
+++ src/main.cpp
@@ -1,127 +1,189 @@
 #include "SDL.h"
+
 #include <iostream>
 
-#ifndef __ANDROID__
-#include "glad/gl.h"
-#else
+#ifdef __ANDROID__
 #include <GLES3/gl3.h>
-#define glClearDepth(color) glClearDepthf(color)
+#define glClearDepth glClearDepthf
+#else
+#include "glad/gl.h"
 #endif
 
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_sdl.h"
 
+#include "fonts/IconsFontAwesome6.h"
+#include "fonts/IconsFontAwesome6.h_fa-solid-900.ttf.h"
+#include "fonts/CaskaydiaCoveNerdFont.h"
+
 SDL_Window *window = nullptr;
 SDL_GLContext glContext = nullptr;
 
-void renderFrame();
 void drawUi();
+void renderFrame();
 
-int main(int argc, char **argv) {
-  SDL_Init(SDL_INIT_EVERYTHING);
+static constexpr char *imgui_settings_file_name = "imgui.ini";
+bool load_imgui_settings_file();
+bool save_imgui_settings_file();
 
-  window =
-      SDL_CreateWindow("CrossGui Window", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 600, 800, SDL_WINDOW_OPENGL);
+int main(int argc, char **argv)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
 
-  glContext = SDL_GL_CreateContext(window);
+    window =
+        SDL_CreateWindow("CrossGui Window", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, 600, 800, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    glContext = SDL_GL_CreateContext(window);
 
 #ifndef __ANDROID__
-  gladLoaderLoadGL();
+    gladLoaderLoadGL();
 #endif
 
-  glClearDepth(1.0);
+    glClearDepth(1.0);
 
-  glEnable(GL_DEPTH_TEST);
-  SDL_GL_SetSwapInterval(1);
+    glEnable(GL_DEPTH_TEST);
+    SDL_GL_SetSwapInterval(1);
 
-  ImGui::CreateContext();
-  ImGuiIO &io = ImGui::GetIO();
-  
-  auto* iniFileIn = SDL_RWFromFile("imgui.ini", "r+b");
-  if(iniFileIn != nullptr){
-    //SDL_RWread()
-    char iniBuff[4096]{0};
-    size_t readSize = SDL_RWread(iniFileIn, iniBuff, sizeof(char), 4096);
-    ImGui::LoadIniSettingsFromMemory(iniBuff, readSize);
-    SDL_RWclose(iniFileIn);
-  }
-  
-  ImGui_ImplSDL2_InitForOpenGL(window, glContext);
-  ImGui_ImplOpenGL3_Init();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
 
-  bool running = true;
-  while (running) {
-    SDL_Event event;
+    load_imgui_settings_file();
 
-    while (SDL_PollEvent(&event)) {
-      ImGui_ImplSDL2_ProcessEvent(&event);
+    ImGui_ImplSDL2_InitForOpenGL(window, glContext);
+    ImGui_ImplOpenGL3_Init();
 
-      switch (event.type) {
-      case SDL_QUIT:
-        running = false;
-        break;
+#ifdef __ANDROID__
+    static constexpr float font_size = 24;
+#else
+    static constexpr float font_size = 16;
+#endif
 
-      case SDL_KEYDOWN: {
-        if (event.key.keysym.sym == SDLK_ESCAPE) {
-          running = false;
+    constexpr ImWchar icon_ranges[] = {ICON_MIN_FA, ICON_MAX_FA, 0};
+    ImFontConfig nerdFontConfig{};
+    nerdFontConfig.MergeMode = true;
+    auto *nerdFont = io.Fonts->AddFontFromMemoryCompressedTTF(caskaydia_cove_nerd_bin_compressed_data, font_size, 20);
+    auto *fontAwesome = io.Fonts->AddFontFromMemoryTTF((void *)s_fa_solid_900_ttf, font_size, 20, &nerdFontConfig, icon_ranges);
+
+    bool running = true;
+
+    bool recTextInput = false;
+    while (running)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (io.WantTextInput)
+            {
+                if (!recTextInput)
+                {
+                    recTextInput = true;
+                    SDL_StartTextInput();
+                }
+            }
+            else
+            {
+                SDL_StopTextInput();
+                recTextInput = false;
+            }
+
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+
+            case SDL_KEYDOWN:
+            {
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                {
+                    running = false;
+                }
+                break;
+            }
+
+            case SDL_MOUSEBUTTONDOWN:
+            {
+                break;
+            }
+
+            default:
+                break;
+            }
         }
-        break;
-      }
-
-      case SDL_MOUSEBUTTONDOWN: {
-        break;
-      }
-
-      default:
-        break;
-      }
+        drawUi();
+        renderFrame();
     }
-    drawUi();
-    renderFrame();
-  }
-  size_t iniSize = 0;
-  auto* iniData = ImGui::SaveIniSettingsToMemory(&iniSize);
-  auto* iniFile = SDL_RWFromFile("imgui.ini", "w+b");
-  if (iniFile != nullptr){
-    SDL_RWwrite(iniFile, iniData, sizeof(char), iniSize);
-    SDL_RWclose(iniFile);
-  }
 
-  //ImGui_ImplSDL2
-  SDL_Quit();
+    save_imgui_settings_file();
 
-  return 0;
+    SDL_Quit();
+
+    return 0;
 }
 
 ImVec4 clearColor = {0.5, 0.2, 0.1, 1.0};
 
-void renderFrame() {
-  ImGui::Render();
+void renderFrame()
+{
+    ImGui::Render();
 
-  glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-  SDL_GL_SwapWindow(window);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    SDL_GL_SwapWindow(window);
 }
 
-char inputText[128] = "Text Default.";
-void drawUi() {
-  ImGui_ImplOpenGL3_NewFrame();
-  ImGui_ImplSDL2_NewFrame();
-  ImGui::NewFrame();
+void drawUi()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
 
-  {
-    ImGui::Begin("Hello Window", NULL, ImGuiWindowFlags_AlwaysAutoResize);
-#ifdef __ANDROID__
-    ImGui::SetWindowFontScale(4);
-#endif
+    {
+        static char inputText[128] = u8"";
+        ImGui::Begin(u8"Hello Window");
 
-    ImGui::Text("Textinho doido");
-    ImGui::InputText("Entre um texto", inputText, 128);
-    ImGui::ColorPicker4("Clear Color", (float *)&clearColor);
-    ImGui::End();
-  }
+        ImGui::Text(u8"FontAwesome Icons -> " ICON_FA_GEAR ICON_FA_BARS);
+        ImGui::InputText(u8"Some Text Input", inputText, 128);
+        ImGui::ColorPicker4(u8"GL Clear Color", (float *)&clearColor);
+
+        ImGui::End();
+    }
+}
+
+bool load_imgui_settings_file()
+{
+    auto *iniFileIn = SDL_RWFromFile(imgui_settings_file_name, "r+b");
+    if (iniFileIn == nullptr)
+        return false;
+
+    char iniBuff[4096]{0};
+    size_t readSize = SDL_RWread(iniFileIn, iniBuff, sizeof(char), 4096);
+    if (readSize == 0)
+        return false;
+
+    ImGui::LoadIniSettingsFromMemory(iniBuff, readSize);
+    SDL_RWclose(iniFileIn);
+
+    return true;
+}
+
+bool save_imgui_settings_file()
+{
+    size_t iniSize = 0;
+    auto *iniData = ImGui::SaveIniSettingsToMemory(&iniSize);
+
+    auto *iniFile = SDL_RWFromFile(imgui_settings_file_name, "w+b");
+    if (iniFile == nullptr)
+        return false;
+
+    if (SDL_RWwrite(iniFile, iniData, sizeof(char), iniSize) < iniSize)
+        return false;
+
+    SDL_RWclose(iniFile);
 }
